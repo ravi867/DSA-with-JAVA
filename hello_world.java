f36739a3d0f6c59bb6d613cc973fed1a6f7eddba
ravi bhatt <112262206+ravi867@users.noreply.github.com>
2024-02-08 19:01:47 +0530

    Create hello_world.java

---
+++ hello_world.java
@@ -0,0 +1,16 @@
+package com.ravi;
+
+public class Hello_world {
+
+// public -> we can access this class from anywhere in our program
+// class-> Itʻs a template. where we can create our datatype
+// Hello_world -> Itʻs a class name.
+  
+    public static void main(String[] args) {
+      // ststic-> we can access this class without creating any object of this class.
+      // void -> Itʻs the return type of our main function.
+      // main-> Entry point of any java program.
+      // String[]-> array of the strings.
+        System.out.println("Hello World!");
+    }
+}
