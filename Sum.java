d5f2ac53e43afeeb067410e106c04718284c017d
ravi bhatt <112262206+ravi867@users.noreply.github.com>
2024-02-09 21:18:46 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,13 @@
+package com.ravi;
+
+import java.util.*;
+public class Sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the two values that you want to add");
+        int a= sc.nextInt();
+        int b= sc.nextInt();
+        System.out.println("the sum of the two number is "+(a+b));
+
+    }
+}
