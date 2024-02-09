722946c09451c20578a7187a4fff1a68ad409f50
ravi bhatt <112262206+ravi867@users.noreply.github.com>
2024-02-09 22:48:10 +0530

    Add files via upload

---
+++ Reverse_of_number.java
@@ -0,0 +1,19 @@
+package com.ravi;
+
+import java.util.Scanner;
+
+public class Reverse_of_number {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter the number");
+        int num= sc.nextInt();
+        int reverse=0;
+        while(num>0)
+        {
+            int rem=num%10;
+            reverse= reverse*10+rem;
+            num=num/10;
+        }
+        System.out.println("the reverse of the nunber is "+reverse);
+    }
+}
